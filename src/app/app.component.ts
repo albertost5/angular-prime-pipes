ae853314c287297a62e9fb1e00c28b2fa1dc1454
albertost5 <sancheztorresalberto@gmail.com>
2024-01-20 21:52:48 +0100

    Add primeng and primeicons packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PrimeNGConfig} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class AppComponent {
-  title = 'angular-prime-pipes';
+  constructor(private readonly primeNgConfig: PrimeNGConfig) {
+    this.primeNgConfig.ripple = true;
+  }
 }
