786826f4b78258374a9256f65cc9e98fca661a5b
albertost5 <sancheztorresalberto@gmail.com>
2024-01-24 18:32:54 +0100

    Add locale to the app, dates in basics-page, numbers section and structure for uncommon

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -10,4 +10,6 @@ export class BasicsPageComponent {
   public nameLowercase: string = 'john doe';
   public nameUpperCase: string = 'JOHN DOE';
   public nameTitleCase: string = 'JoHn DoE';
+
+  public customDate: Date = new Date();
 }
--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'products-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class NumbersPageComponent {
-
+  public totalSales: number = 100;
+  public percent: number = 0.7;
 }
