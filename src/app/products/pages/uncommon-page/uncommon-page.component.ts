522c6dd9b4f149975e42be9541bec2d7d7376a3c
albertost5 <sancheztorresalberto@gmail.com>
2024-01-25 19:06:16 +0100

    Added i18n, slice, json, keyvalue and async pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'products-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class UncommonPageComponent {
+  // i18nSelect
+  public name: string = 'John Doe';
+  public gender: 'male' | 'female' = 'male';
+  public inviteMap = {
+    'male': 'His',
+    'female': 'Her'
+  }
 
+  public togglePerson(): void {
+    if ( this.gender == 'male' ) {
+      this.name = 'Lisa Simpson';
+      this.gender = 'female';
+    } else {
+      this.name = 'John Doe';
+      this.gender = 'male';
+    }
+  }
+
+  // i18nPlural
+  public clients: string[] = ['John', 'Ferris', 'X'];
+
+  public deleteClient(): void {
+    this.clients.pop();
+  }
+
+  public messageMapping = {
+    '=0': 'There are no clients',
+    '=1': 'There is one client',
+    'other': 'There are # clients'
+  }
+
+  // KeyValuePipe
+  public person = {
+    name: 'John',
+    age: 30,
+    country: 'Spain'
+  }
+
+  // Async Pipe
+  public timer$ = interval(1000);
 }
